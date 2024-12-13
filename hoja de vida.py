7e302ecd294d1266190867fa2fbafcb01d807126
USER <pipegallo04@gmail.com>
2024-12-13 07:21:51 -0500

    first commit

---
+++ hoja de vida.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Mi Primera App con Streamlit")
+st.write("¡Hola, esta es mi primera aplicación interactiva!")
+
+if st.button("Presiona aquí"):
+    st.success("¡Botón presionado!")
